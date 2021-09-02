11b5d79c15ec54abaf6e658da6fab1f7de8f86b0
Camilly Gama <camysgama@outlook.com>
2021-09-01 22:14:23 -0300

    fit: Calculate Controller

---
+++ src/controllers/CalculateController.ts
@@ -0,0 +1,20 @@
+import { Request, Response} from "express";
+import { CalculateService } from "../services/CalculateService";
+
+class CalculateController {
+
+    async handle(request: Request, response: Response) {
+
+        const {origin, destiny, time, plan} = request.body;
+
+        const calculateService = new CalculateService();
+
+        const user = await calculateService.execute({origin, destiny, time, plan});
+        
+        //retornando as informações que foram recuperadas
+        return response.json(user);
+    }
+
+}
+
+export { CalculateController}
