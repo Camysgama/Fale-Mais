cece568da6def09120c95d32fc789a3083bd68ca
Camilly Gama <camysgama@outlook.com>
2021-09-04 16:39:29 -0300

    Feat: Calculate Tariff and Prices

--- src/services/CalculateService.ts
+++ src/services/CalculateService.ts
@@ -1,28 +1,24 @@
 
 import { getCustomRepository } from "typeorm";
 import { UserRepository } from "../repositories/UserRepository"
-import { TarifService } from "./TarifService";
 
- 
 
- interface  IUserRequest{
+ //dados que terão que ser requeridos e executados pela classe  
+interface  IUserRequest{
      origin: number;
      destiny: number;
      time: number;
      plan: number
  }
- //dados que terão que ser requeridos e executados pela classe   
- 
- class CalculateService extends TarifService{ 
+
+ class CalculateService{ 
      async execute({origin, destiny, time, plan}: IUserRequest) {
          const userRepository = getCustomRepository(UserRepository); //instanciando repositório
 
         if(!origin || !destiny ||  !time || !plan) {
            throw new Error("Insira corretamente o campo");
         }else {
-
-         //const tarif = TarifService.calculate(origin, destiny);
-
+         
          //criar dados dentro da base de dados
          const passData = userRepository.create({
             origin,
@@ -32,14 +28,9 @@ import { TarifService } from "./TarifService";
         })
 
         await userRepository.save(passData);
-
         return passData;
         }
-
      }
-
  }
 
-
  export { CalculateService }
- //recebe os dados da req e 
